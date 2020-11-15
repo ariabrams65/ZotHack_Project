0dd4213d9032aba5ba06e99cb6a3c7fd901fa2d2
angela4321 <angelaqiao621@gmail.com>
2020-11-14 16:15:23 -0800

    Add images to descriptions

--- src/components/climate.js
+++ src/components/climate.js
@@ -1,13 +1,31 @@
 import React, {useState, useEffect} from 'react';
+import Clear from '../images/clear.png'
+import Cloud from '../images/cloud.png'
+import Rain from '../images/rain.png'
+import Snow from '../images/snow.png'
 //function DisplayWeather(props) {
 const DisplayWeather = (props) => {
     const { REACT_APP_API_KEY } = process.env;
     async function getWeather(){
       let res = await fetch("http://api.openweathermap.org/data/2.5/weather?q="+props.location+"&appid="+REACT_APP_API_KEY+"&mode=json")
       let data = await res.json()
-
+      let image = null
+      if (data.weather[0].main==="Clear"){
+        image = Clear
+      }
+      else if(data.weather[0].main==="Clouds"){
+        image = Cloud
+      }
+      else if(data.weather[0].main==="Rain"){
+        image = Rain
+      }
+      else if(data.weather[0].main==="Snow"){
+        image = Snow
+      }
       let weather = {
-        temp: data.weather[0].main
+        type: data.weather[0].main,
+        description: data.weather[0].description,
+        image: image
       }
       return weather
     }  
@@ -23,7 +41,10 @@ const DisplayWeather = (props) => {
 
     return (
       <div>
-        <div> {token!=null?token.temp:null}</div>
+        <div> {token!=null?token.type:null}</div>
+
+        <div> {token!=null?token.description:null}</div>
+        <img src={token!=null?token.image:null} width = "100" height = "100"/>
       </div>
     );
   }
